78c854a5ce2415a178647dfc78aa62fffb5b3c4e
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2026-06-04 01:59:23 +0200

    useless comment

--- packages/contracts/events/domain.ts
+++ packages/contracts/events/domain.ts
@@ -5,6 +5,5 @@ export { DomainEventMeta } from "@yodea/contracts/events/meta"
 
 export const DomainEvent = Schema.Union(Object.values(ProjectEvent.cases)).pipe(Schema.toTaggedUnion("_tag"))
 export type DomainEvent = typeof DomainEvent.Type
-export type DomainEventEncoded = Schema.Codec.Encoded<typeof DomainEvent>
 
 export const DomainEventFromJson = Schema.fromJsonString(DomainEvent)
