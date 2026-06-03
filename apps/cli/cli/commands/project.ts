e19205d0a8072552637b5bf29e0860eb08c5eb18
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2026-06-03 20:59:19 +0200

    feat(cli): project delete <name|id> command

--- apps/cli/cli/commands/project.ts
+++ apps/cli/cli/commands/project.ts
@@ -6,8 +6,9 @@ import { changeDirectoryCommand } from "@yodea/cli/commands/project/change-direc
 import { archiveCommand } from "@yodea/cli/commands/project/archive"
 import { restoreCommand } from "@yodea/cli/commands/project/restore"
 import { setMetadataCommand } from "@yodea/cli/commands/project/set-metadata"
+import { deleteCommand } from "@yodea/cli/commands/project/delete"
 
 export const projectCommand = Command.make("project").pipe(
   Command.withDescription("Manage projects"),
-  Command.withSubcommands([createCommand, listCommand, renameCommand, changeDirectoryCommand, archiveCommand, restoreCommand, setMetadataCommand])
+  Command.withSubcommands([createCommand, listCommand, renameCommand, changeDirectoryCommand, archiveCommand, restoreCommand, setMetadataCommand, deleteCommand])
 )
